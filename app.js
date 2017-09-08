78fcbb4d4d5e7f77932c73d456f3f300f2841584
Lim Wu Shen <xiao.wsl@gmail.com>
2017-09-08 20:04:43 +0800

    new

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+var http = require('http');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
 });
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: '311fc6b0-685b-4fd0-b49e-c77e46008777',
-    appPassword: '215tbbjeUvDHK0cP1FFs9px'
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+console.log("Server running at http://localhost:%d", port);
