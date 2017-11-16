676d13ea355bcbeaf1495f682d952ce3e670f657
Lim Wu Shen <xiao.wsl@gmail.com>
2017-11-16 14:39:47 +0800

    ss

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+let authentication = require("./authentication");
+var bookingInfo = [];
+var submit = [];
+var test = [];
+var row;
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -11,6 +18,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     appId: '311fc6b0-685b-4fd0-b49e-c77e46008777',
     appPassword: '215tbbjeUvDHK0cP1FFs9px'
+
 });
 server.post('/api/messages', connector.listen());
 
@@ -176,17 +184,100 @@ bot.dialog('greetings', [
     matches: /^(cancel|nevermind)/i
 });
 
+bot.dialog('readbooking', 
+    function (session) {
+        session.send('read');
+        getauth1(session);
+    }
+).triggerAction({
+    matches: 'Read.Ss'
+}).cancelAction('cancelReadNote', "Ok.", {
+    matches: /^(cancel|nevermind)/i
+});
+
 bot.dialog('booking', [
     // Step 1
     function (session) {
-        builder.Prompts.text(session, 'How many people you need to book??');
+        builder.Prompts.text(session, '%s How many people you need to book??');
     },
     // Step 2
     function (session, results) {
-        session.endDialog(` ${results.response} slot was booked!`);
+        bookingInfo.push(results.response);
+        session.send(' %s slot booked',results.response);
+        builder.Prompts.text(session, 'Which set meal you wish to book?'); 
+    },
+    // Step 3
+    function (session, results) {
+        bookingInfo.push(results.response);
+        session.send(' %s set meal booked. ',results.response);
+        session.endDialog('Thanks for booking, our staff will contact you as soon as possible.');     
+        getauth(session,bookingInfo);
     }
 ]).triggerAction({
     matches: 'Restaurant.Book'
 }).cancelAction('cancelReadNote', "Ok.", {
     matches: /^(cancel|nevermind)/i
 });
+
+function getauth(session,submit){
+    authentication.authenticate().then((auth)=>{
+        appendData(auth,submit);
+      });
+
+}
+
+function getauth1(session){
+    authentication.authenticate().then((auth)=>{
+        updatess(auth,session);
+      });
+}
+
+function updatess(auth,session) {
+    var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+      spreadsheetId: '15M6lPILyUsc4o26ZtnTY6HvJM5aBix4VqR76fh1ftXU',
+      range: 'Sheet1!A2:C', //Change Sheet1 if your worksheet's name is something else
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+      } 
+      var rows = response.values;
+      if (rows.length == 0) {
+        console.log('No data found.');
+        session.send('no data found');
+      } else {
+        for (var i = 0; i < rows.length; i++) {
+         row = rows[i];
+          test.push(row.join(", ")) ;
+          console.log(row.join(", "));
+        }
+    }
+    var string = test.toString();
+    session.send('%s',string);
+    });
+}
+
+function appendData(auth,submit) {
+    var sheets = google.sheets('v4');
+    var hello = submit;
+    sheets.spreadsheets.values.append({
+      auth: auth,
+      spreadsheetId: '15M6lPILyUsc4o26ZtnTY6HvJM5aBix4VqR76fh1ftXU',
+      range: 'sheet1!A2:C', //Change Sheet1 if your worksheet's name is something else
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [ hello ]
+      }
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+      } else {
+          console.log("Appended");
+      }
+    });
+}
+
+function myFunc(arg) {
+    console.log(`arg was => ${arg}`);
+  }
--- luis.js
+++ luis.js
@@ -1,6 +1,15 @@
+
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
 var builder = require('botbuilder');
 var restify = require('restify');
-
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+let authentication = require("./authentication");
+var row;
+var array;
+var hello = [];
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -9,184 +18,247 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot and listen to messages
 var connector = new builder.ChatConnector({
-    appId: '311fc6b0-685b-4fd0-b49e-c77e46008777',
-    appPassword: '215tbbjeUvDHK0cP1FFs9px'
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send("Hi... I'm the note bot sample. I can create new notes, read saved notes to you and delete notes.");
-
-   // If the object for storing notes in session.userData doesn't exist yet, initialize it
-   if (!session.userData.notes) {
-       session.userData.notes = {};
-       console.log("initializing userData.notes in default message handler");
-   }
-});
-
-// Add global LUIS recognizer to bot
-var luisAppUrl = process.env.LUIS_APP_URL || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/5cc79349-2318-4b90-a410-9bec5a5c0d58?subscription-key=2544402025834de886a796709461919d&timezoneOffset=0&verbose=true&q=';
-bot.recognizer(new builder.LuisRecognizer(luisAppUrl));
-
-// CreateNote dialog
-bot.dialog('CreateNote', [
-    function (session, args, next) {
-        // Resolve and store any Note.Title entity passed from LUIS.
-        var intent = args.intent;
-        var title = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-
-        var note = session.dialogData.note = {
-          title: title ? title.entity : null,
-        };
-        
-        // Prompt for title
-        if (!note.title) {
-            builder.Prompts.text(session, 'What would you like to call your note?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var note = session.dialogData.note;
-        if (results.response) {
-            note.title = results.response;
-        }
-
-        // Prompt for the text of the note
-        if (!note.text) {
-            builder.Prompts.text(session, 'What would you like to say in your note?');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        var note = session.dialogData.note;
-        if (results.response) {
-            note.text = results.response;
-        }
-        
-        // If the object for storing notes in session.userData doesn't exist yet, initialize it
-        if (!session.userData.notes) {
-            session.userData.notes = {};
-            console.log("initializing session.userData.notes in CreateNote dialog");
-        }
-        // Save notes in the notes object
-        session.userData.notes[note.title] = note;
-
-        // Send confirmation to user
-        session.endDialog('Creating note named "%s" with text "%s"',
-            note.title, note.text);
-    }
-]).triggerAction({ 
-    matches: 'Note.Create',
-    confirmPrompt: "This will cancel the creation of the note you started. Are you sure?" 
-}).cancelAction('cancelCreateNote', "Note canceled.", {
-    matches: /^(cancel|nevermind)/i,
-    confirmPrompt: "Are you sure?"
-});
-
-// Delete note dialog
-bot.dialog('DeleteNote', [
-    function (session, args, next) {
-        if (noteCount(session.userData.notes) > 0) {
-            // Resolve and store any Note.Title entity passed from LUIS.
-            var title;
-            var intent = args.intent;
-            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-            if (entity) {
-                // Verify that the title is in our set of notes.
-                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
-            }
-            
-            // Prompt for note name
-            if (!title) {
-                builder.Prompts.choice(session, 'Which note would you like to delete?', session.userData.notes);
-            } else {
-                next({ response: title });
-            }
-        } else {
-            session.endDialog("No notes to delete.");
-        }
-    },
-    function (session, results) {
-        delete session.userData.notes[results.response.entity];        
-        session.endDialog("Deleted the '%s' note.", results.response.entity);
-    }
-]).triggerAction({
-    matches: 'Note.Delete'
-}).cancelAction('cancelDeleteNote', "Ok - canceled note deletion.", {
-    matches: /^(cancel|nevermind)/i
-});
-
-// Helper function to count the number of notes stored in session.userData.notes
-function noteCount(notes) {
-    
-        var i = 0;
-        for (var name in notes) {
-            i++;
-        }
-        return i;
-    }
-
-    // Read note dialog
-bot.dialog('ReadNote', [
-    function (session, args, next) {
-        if (noteCount(session.userData.notes) > 0) {
-           
-            // Resolve and store any Note.Title entity passed from LUIS.
-            var title;
-            var intent = args.intent;
-            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-            if (entity) {
-                // Verify it's in our set of notes.
-                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
-            }
-            
-            // Prompt for note name
-            if (!title) {
-                builder.Prompts.choice(session, 'Which note would you like to read?', session.userData.notes);
-            } else {
-                next({ response: title });
-            }
-        } else {
-            session.endDialog("No notes to read.");
-        }
-    },
-    function (session, results) {        
-        session.endDialog("Here's the '%s' note: '%s'.", results.response.entity, session.userData.notes[results.response.entity].text);
-    }
-]).triggerAction({
-    matches: 'Note.ReadAloud'
-}).cancelAction('cancelReadNote', "Ok.", {
-    matches: /^(cancel|nevermind)/i
-});
-
-bot.dialog('greetings', [
-    // Step 1
+var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        builder.Prompts.choice(session, 'What card would like to test?', CardNames, {
+            maxRetries: 3,
+            retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
+        });
     },
-    // Step 2
     function (session, results) {
-        session.endDialog(`Hello ${results.response}!`);
-    }
-]).triggerAction({
-    matches: 'greetings'
-}).cancelAction('cancelReadNote', "Ok.", {
-    matches: /^(cancel|nevermind)/i
-});
 
-bot.dialog('booking', [
-    // Step 1
-    function (session) {
-        builder.Prompts.text(session, 'How many people you need to book??');
-    },
-    // Step 2
-    function (session, results) {
-        session.endDialog(` ${results.response} slot was booked!`);
+        // create the card based on selection
+        var selectedCardName = results.response.entity;
+        var card = createCard(selectedCardName, session);
+
+        // attach the card to the reply message
+        var msg = new builder.Message(session).addAttachment(card);
+        session.send(msg);
+        session.send("hi"+session.message.user.name);
+        
     }
-]).triggerAction({
-    matches: 'Restaurant.Book'
-}).cancelAction('cancelReadNote', "Ok.", {
-    matches: /^(cancel|nevermind)/i
-});
+]);
+
+var HeroCardName = 'Hero card';
+var ThumbnailCardName = 'Thumbnail card';
+var ReceiptCardName = 'Receipt card';
+var SigninCardName = 'Sign-in card';
+var AnimationCardName = "Animation card";
+var VideoCardName = "Video card";
+var AudioCardName = "Audio card";
+var CardNames = [HeroCardName, ThumbnailCardName, ReceiptCardName, SigninCardName, AnimationCardName, VideoCardName, AudioCardName];
+
+function createCard(selectedCardName, session) {
+    switch (selectedCardName) {
+        case HeroCardName:
+            return createHeroCard(session);
+        case ThumbnailCardName:
+            return createThumbnailCard(session);
+        case ReceiptCardName:
+            return createReceiptCard(session);
+        case SigninCardName:
+            return createSigninCard(session);sss
+        case AnimationCardName:
+            return createAnimationCard(session);
+        case VideoCardName:
+            return createVideoCard(session);
+        case AudioCardName:
+            return getauth(session);
+        default:
+            return createHeroCard(session);
+    }
+}
+
+function createHeroCard(session) {
+
+    return new builder.HeroCard(session)
+        .title('BotFramework Hero Card ')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images([
+            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+        .images([
+            builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png')
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
+        ]);
+}
+
+function createThumbnailCard(session) {
+    var cards = new builder.ThumbnailCard(session)
+    .title('BotFramework Thumbnail Card')
+    .subtitle('Your bots — wherever your users are talking')
+    .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+    .images([
+        builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+    ])
+    .buttons([
+        builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
+    ]);
+    for(i=0;i<2;i++){
+    return cards;
+}
+}
+
+var order = 1234;
+function createReceiptCard(session) {
+    return new builder.ReceiptCard(session)
+        .title('John Doe')
+        .facts([
+            builder.Fact.create(session, order++, 'Order Number'),
+            builder.Fact.create(session, 'VISA 5555-****', 'Payment Method')
+        ])
+        .items([
+            builder.ReceiptItem.create(session, '$ 38.45', 'Data Transfer')
+                .quantity(368)
+                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png')),
+            builder.ReceiptItem.create(session, '$ 45.00', 'App Service')
+                .quantity(720)
+                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
+        ])
+        .tax('$ 7.50')
+        .total('$ 90.95')
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
+                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
+        ]);
+}
+
+function createSigninCard(session) {
+    return new builder.SigninCard(session)
+        .text('BotFramework Sign-in Card')
+        .button('Sign-in', 'https://login.microsoftonline.com');
+}
+
+function createAnimationCard(session) {
+    return new builder.AnimationCard(session)
+        .title('Microsoft Bot Framework')
+        .subtitle('Animation Card')
+        .image(builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png'))
+        .media([
+            { url: 'http://i.giphy.com/Ki55RUbOV5njy.gif' }
+        ]);
+}
+
+function createVideoCard(session) {
+    return new builder.VideoCard(session)
+        .title('Big Buck Bunny')
+        .subtitle('by the Blender Institute')
+        .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.')
+        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
+        .media([
+            { url: 'https://www.youtube.com/watch?v=L2uETng8peg' }
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://peach.blender.org/', 'Learn More')
+        ]);
+}
+
+function createAudioCard(session) {
+    return new builder.AudioCard(session)
+        .title('I am your father')
+        .subtitle('Star Wars: Episode V - The Empire Strikes Back')
+        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
+        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'))
+        .media([
+            { url: 'http://www.wavlist.com/movies/004/father.wav' }
+        ])        .buttons([
+            builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
+        ]);
+}   
+
+function getauth(session){
+    authentication.authenticate().then((auth)=>{
+        updatess(auth);
+      });
+      if(hello.length>0){
+      return new builder.ThumbnailCard(session)
+      .title('BotFramework Thumbnail Card')
+      .subtitle('Your bots — wherever your users are talking')
+      .text('asd')
+      .images([
+          builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+      ])
+      .buttons([
+          builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
+      ]);
+    }
+    else{
+         session.send('test');
+    }
+
+}
+function updatess(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+      auth: auth,
+      spreadsheetId: '15M6lPILyUsc4o26ZtnTY6HvJM5aBix4VqR76fh1ftXU',
+      range: 'Sheet1!A2:C', //Change Sheet1 if your worksheet's name is something else
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+      } 
+      var rows = response.values;
+      if (rows.length === 0) {
+        console.log('No data found.');
+      } else {
+        for (var i = 0; i < rows.length; i++) {
+          row = rows[i];
+          hello.push(row.join(", ")) ;
+          console.log(row.join(", "));
+        }
+    }
+    });
+}
+
+function appendData(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.append({
+      auth: auth,
+      spreadsheetId: '15M6lPILyUsc4o26ZtnTY6HvJM5aBix4VqR76fh1ftXU',
+      range: 'sheet1!A2:C', //Change Sheet1 if your worksheet's name is something else
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [ hello ]
+      }
+    }, (err, response) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return session.send('error');
+      } else {
+            session.send("succesfully updated =D");
+          console.log("Appended");
+      }
+    });
+  }
+
+  bot.dialog('showShirts', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Classic White T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+            ]),
+        new builder.HeroCard(session)
+            .title("Classic Gray T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+            ])
+    ]);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: /^(show|list)/i });
