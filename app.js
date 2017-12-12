cd154b58b3133b5e631f803227c5ee3fad56fcef
Lim Wu Shen <xiao.wsl@gmail.com>
2017-12-13 00:54:58 +0800

    'int12'

--- app.js
+++ app.js
@@ -17,159 +17,22 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot and listen to messages
 var connector = new builder.ChatConnector({
+
+
     appId: '311fc6b0-685b-4fd0-b49e-c77e46008777',
     appPassword: '215tbbjeUvDHK0cP1FFs9px'
 
-
 });
+
 server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send("Hi... I'm the note bot sample. I can create new notes, read saved notes to you and delete notes.");
-
-   // If the object for storing notes in session.userData doesn't exist yet, initialize it
-   if (!session.userData.notes) {
-       session.userData.notes = {};
-       console.log("initializing userData.notes in default message handler");
-   }
+    session.send("Hi... I'm the robot for restaurant, You can chat with me to get information of the restaurant.");
 });
 
 // Add global LUIS recognizer to bot
 var luisAppUrl = process.env.LUIS_APP_URL || 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/5cc79349-2318-4b90-a410-9bec5a5c0d58?subscription-key=2544402025834de886a796709461919d&timezoneOffset=0&verbose=true&q=';
 bot.recognizer(new builder.LuisRecognizer(luisAppUrl));
-
-// CreateNote dialog
-bot.dialog('CreateNote', [
-    function (session, args, next) {
-        // Resolve and store any Note.Title entity passed from LUIS.
-        var intent = args.intent;
-        var title = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-
-        var note = session.dialogData.note = {
-          title: title ? title.entity : null,
-        };
-        
-        // Prompt for title
-        if (!note.title) {
-            builder.Prompts.text(session, 'What would you like to call your note?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var note = session.dialogData.note;
-        if (results.response) {
-            note.title = results.response;
-        }
-
-        // Prompt for the text of the note
-        if (!note.text) {
-            builder.Prompts.text(session, 'What would you like to say in your note?');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        var note = session.dialogData.note;
-        if (results.response) {
-            note.text = results.response;
-        }
-        
-        // If the object for storing notes in session.userData doesn't exist yet, initialize it
-        if (!session.userData.notes) {
-            session.userData.notes = {};
-            console.log("initializing session.userData.notes in CreateNote dialog");
-        }
-        // Save notes in the notes object
-        session.userData.notes[note.title] = note;
-
-        // Send confirmation to user
-        session.endDialog('Creating note named "%s" with text "%s"',
-            note.title, note.text);
-    }
-]).triggerAction({ 
-    matches: 'Note.Create',
-    confirmPrompt: "This will cancel the creation of the note you started. Are you sure?" 
-}).cancelAction('cancelCreateNote', "Note canceled.", {
-    matches: /^(cancel|nevermind)/i,
-    confirmPrompt: "Are you sure?"
-});
-
-// Delete note dialog
-bot.dialog('DeleteNote', [
-    function (session, args, next) {
-        if (noteCount(session.userData.notes) > 0) {
-            // Resolve and store any Note.Title entity passed from LUIS.
-            var title;
-            var intent = args.intent;
-            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-            if (entity) {
-                // Verify that the title is in our set of notes.
-                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
-            }
-            
-            // Prompt for note name
-            if (!title) {
-                builder.Prompts.choice(session, 'Which note would you like to delete?', session.userData.notes);
-            } else {
-                next({ response: title });
-            }
-        } else {
-            session.endDialog("No notes to delete.");
-        }
-    },
-    function (session, results) {
-        delete session.userData.notes[results.response.entity];        
-        session.endDialog("Deleted the '%s' note.", results.response.entity);
-    }
-]).triggerAction({
-    matches: 'Note.Delete'
-}).cancelAction('cancelDeleteNote', "Ok - canceled note deletion.", {
-    matches: /^(cancel|nevermind)/i
-});
-
-// Helper function to count the number of notes stored in session.userData.notes
-function noteCount(notes) {
-    
-        var i = 0;
-        for (var name in notes) {
-            i++;
-        }
-        return i;
-    }
-
-    // Read note dialog
-bot.dialog('ReadNote', [
-    function (session, args, next) {
-        if (noteCount(session.userData.notes) > 0) {
-           
-            // Resolve and store any Note.Title entity passed from LUIS.
-            var title;
-            var intent = args.intent;
-            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
-            if (entity) {
-                // Verify it's in our set of notes.
-                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
-            }
-            
-            // Prompt for note name
-            if (!title) {
-                builder.Prompts.choice(session, 'Which note would you like to read?', session.userData.notes);
-            } else {
-                next({ response: title });
-            }
-        } else {
-            session.endDialog("No notes to read.");
-        }
-    },
-    function (session, results) {        
-        session.endDialog("Here's the '%s' note: '%s'.", results.response.entity, session.userData.notes[results.response.entity].text);
-    }
-]).triggerAction({
-    matches: 'Note.ReadAloud'
-}).cancelAction('cancelReadNote', "Ok.", {
-    matches: /^(cancel|nevermind)/i
-});
 
 bot.dialog('greetings', [
     // Step 1
@@ -189,7 +52,6 @@ bot.dialog('greetings', [
 bot.dialog('readbooking', 
     function (session) {
         session.send('read  %s',msg);
-        getauth1(session);
     }
 ).triggerAction({
     matches: 'Read.Ss'
@@ -198,6 +60,98 @@ bot.dialog('readbooking',
 });
 
 bot.dialog('booking', [
+    
+    // Step 1
+    function (session) {
+        bookingInfo.push(session.message.user.name);
+        builder.Prompts.text(session, ' How many people you need to book??');
+    },
+    // Step 2
+    function (session, results) {
+        var slot = builder.EntityRecognizer.parseNumber(results.response); 
+        if(!isNaN(slot)){
+        session.send('%s slot booked.',slot);
+        builder.Prompts.text(session, 'Which set meal you wish to book?'); 
+        }
+        else{
+        session.endDialog("Not a number enter, please retry.");
+        }
+    },
+    // Step 3
+    function (session, results) {
+        bookingInfo.push(results.response);
+        session.send(' %s set meal booked. ',results.response);
+        builder.Prompts.text(session,'What is the date that you wish to book?');     
+    },
+    //step 4
+    function (session, results) {
+        bookingInfo.push(results.response);
+        session.send(' Successfully booked! ');
+        session.endDialog('Thanks for booking, our staff will contact you as soon as possible.');    
+        getauth(session,bookingInfo);
+        }
+        
+    }
+]).triggerAction({
+    matches: 'Restaurant.Book'
+}).cancelAction('cancelReadNote', "Ok.", {
+    matches: /^(cancel|nevermind)/i
+});
+
+bot.dialog('test', [
+    // Step 1
+    function (session) {
+        builder.Prompts.text(session, ' How many people you need to book??');
+    },
+    // Step 2
+    function (session, results) {
+        var slot = builder.EntityRecognizer.parseNumber(results.response); 
+        if(!isNaN(slot)){
+        session.send('%s',slot);
+        builder.Prompts.text(session, 'Which set meal you wish to book?'); 
+        }
+        else{
+        session.endDialog("Not a number enter, please retry.");
+        }
+    },
+    // Step 3
+    function (session, results) {
+        session.send(' %s set meal booked. ',results.response);
+        session.endDialog('Thanks for booking, our staff will contact you as soon as possible.');     
+    }
+    
+    
+]).triggerAction({
+    matches: '93939393'
+}).cancelAction('cancelReadNote', "Ok.", {
+    matches: /^(cancel|nevermind)/i
+});
+
+bot.dialog('NewProduct', [
+    // Step 1
+    function (session) {
+        builder.Prompts.text(session, ' How many people you need to book??');
+    },
+    // Step 2
+    function (session, results) {
+        bookingInfo.push(results.response);
+        session.send(' %s slot booked',results.response);
+        builder.Prompts.text(session, 'Which set meal you wish to book?'); 
+    },
+    // Step 3
+    function (session, results) {
+        bookingInfo.push(results.response);
+        session.send(' %s set meal booked. ',results.response);
+        session.endDialog('Thanks for booking, our staff will contact you as soon as possible.');     
+        getauth(session,bookingInfo);
+    }
+]).triggerAction({
+    matches: 'New.Product'
+}).cancelAction('cancelReadNote', "Ok.", {
+    matches: /^(cancel|nevermind)/i
+});
+
+bot.dialog('Recommendation', [
     // Step 1
     function (session) {
         builder.Prompts.text(session, '%s How many people you need to book??');
@@ -216,10 +170,11 @@ bot.dialog('booking', [
         getauth(session,bookingInfo);
     }
 ]).triggerAction({
-    matches: 'Restaurant.Book'
+    matches: 'Recommend.Specialty'
 }).cancelAction('cancelReadNote', "Ok.", {
     matches: /^(cancel|nevermind)/i
 });
+
 
 function getauth(session,submit){
     authentication.authenticate().then((auth)=>{
@@ -235,7 +190,9 @@ function getauth1(session){
       });
 }
 
+
 function updatess(auth,session) {
+    var s ;
     session.send('upatess')
     var sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
@@ -254,7 +211,9 @@ function updatess(auth,session) {
         for (var i = 0; i < rows.length; i++) {
          row = rows[i];
           test.push(row.join(", ")) ;
-          console.log(row.join(", "));
+          console.log(row);
+          s = test.toString();
+          session.send('%s ', s);
         }
     }
     var string = test.toString();
@@ -281,6 +240,13 @@ function appendData(auth,submit) {
       }
     });
 }
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+  });
+  
+  setTimeout(function () {
+    console.log('This will still run.');
+  }, 5000);
 
 function myFunc(arg) {
     console.log(`arg was => ${arg}`);
